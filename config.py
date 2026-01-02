98e89444137c0ae66dcb7b63b544b80820fc19a5
rpbotforg-boop <rpbotforg@gmail.com>
2026-01-02 17:04:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,19 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = "8409425850:AAH_cMt_YrQM_FmJTAjwp8MuOkkN709AVr0"
-    API_ID = 27433400
-    API_HASH = "1a286620de5ffe0a7d9b57e604293555"
-    ADMIN = '6201066540'.split(',')
+    BOT_TOKEN = "8225118430:AAEOFTRcVH3Au1LR0iFyasUb4U5CGATuoT4"
+    API_ID = 22447622
+    API_HASH = "543b62d58d3e723e766ba57a984ab65d"
+    ADMIN = '777756062'.split(',')
     ADMIN_ID = [int(id) for id in ADMIN]
     DB_URL = "mongodb+srv://niravpatel180503_db_user:vjWNaWhRk0gMSNyQ@cluster0.26bfgmf.mongodb.net/?appName=Cluster0"
     DB_NAME = "Crack_Jack"
-    TXT_LOG = -1003595781984
-    AUTH_LOG = -1003595781984
+    TXT_LOG = -1003550815342
+    AUTH_LOG = -1003664908028
     HIT_LOG = -1003595781984
-    DRM_DUMP = -1003595781984
-    CHANNEL = -1003635671191
-    CH_URL = "https://t.me/lifeline2308"
-    OWNER = "https://t.me/Priyansh230819"
+    DRM_DUMP = -1003523745178
+    CHANNEL = -1003481794992
+    CH_URL = "https://t.me/+qTv8z74iwss1Y2U1"
+    OWNER = "https://t.me/@king11up"
     THUMB_URL = "https://vault.pictures/media/images/b9/ad/12/b9ad123d95cd49465fcb954d.jpg" #Replace by with your Thumb URL
     HOST = "https://drm-api-five.vercel.app"
